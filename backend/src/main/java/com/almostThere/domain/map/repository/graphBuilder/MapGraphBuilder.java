f15a37cd5144a26fe4416b15d09fe3c34d1da78a
shortboy7 <thdwldnd7@naver.com>
2023-11-04 00:16:12 +0900

    fix:graph builder interface 변경

--- backend/src/main/java/com/almostThere/domain/map/repository/graphBuilder/MapGraphBuilder.java
+++ backend/src/main/java/com/almostThere/domain/map/repository/graphBuilder/MapGraphBuilder.java
@@ -3,9 +3,8 @@ package com.almostThere.domain.map.repository.graphBuilder;
 import com.almostThere.domain.map.entity.link.MapLink;
 import com.almostThere.domain.map.entity.node.MapNode;
 import com.almostThere.domain.map.repository.MapGraph;
+import java.util.List;
 
 public interface MapGraphBuilder {
-    void addNode(MapNode node);
-    void addEdge(MapLink link);
-    MapGraph build();
+    MapGraph build(List<MapNode> nodes, List<MapLink> links);
 }
