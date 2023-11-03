e4e21f989cd3395b540f4983df4eb01a136b90db
shortboy7 <thdwldnd7@naver.com>
2023-11-03 23:33:16 +0900

    feat:implement MapGraph

--- backend/src/main/java/com/almostThere/domain/map/repository/impl/ListMapGraph.java
+++ backend/src/main/java/com/almostThere/domain/map/repository/impl/ListMapGraph.java
@@ -1,18 +1,28 @@
 package com.almostThere.domain.map.repository.impl;
 
-import com.almostThere.domain.map.entity.MapNode;
+import com.almostThere.domain.map.entity.link.MapLink;
+import com.almostThere.domain.map.entity.node.MapNode;
 import com.almostThere.domain.map.repository.MapGraph;
 import java.util.List;
+import java.util.Map;
 
 public class ListMapGraph implements MapGraph {
-    private List<List<MapNode>> adjacentGraph;
+    private List<MapNode>           actualNode;
+    private Map<Integer, Integer>   map_to_id;
+    private List<List<MapLink>> adjacentGraph;
 
-    public ListMapGraph(List<List<MapNode>> graph) {
-        this.adjacentGraph = graph;
+    public ListMapGraph(
+            List<MapNode> actualNode,
+            Map<Integer, Integer> map_to_id,
+            List<List<MapLink>> adjacentGraph
+        ) {
+        this.actualNode = actualNode;
+        this.map_to_id = map_to_id;
+        this.adjacentGraph = adjacentGraph;
     }
 
     @Override
-    public List<MapNode> getAdjacentNodes(int index) {
+    public List<MapLink> getAdjacentNodes(int index) {
         return this.adjacentGraph.get(index);
     }
 }
