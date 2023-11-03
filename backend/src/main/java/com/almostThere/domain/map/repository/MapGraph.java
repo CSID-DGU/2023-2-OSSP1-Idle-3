9d9e570eb6e69f4fec6b84ab2bcd9b3d4d189197
shortboy7 <thdwldnd7@naver.com>
2023-11-03 23:30:59 +0900

    feat:add map graph interface

--- backend/src/main/java/com/almostThere/domain/map/repository/MapGraph.java
+++ backend/src/main/java/com/almostThere/domain/map/repository/MapGraph.java
@@ -1,8 +1,9 @@
 package com.almostThere.domain.map.repository;
 
-import com.almostThere.domain.map.entity.MapNode;
+import com.almostThere.domain.map.entity.link.MapLink;
+import com.almostThere.domain.map.entity.node.MapNode;
 import java.util.List;
 
 public interface MapGraph {
-    List<MapNode> getAdjacentNodes(int index);
+    List<MapLink> getAdjacentNodes(int index);
 }
