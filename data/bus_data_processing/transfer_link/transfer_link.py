3811d57fe0a6027f0b8c34abbd1835a7bc30cde7
seonwoonam <potatomoney716@gmail.com>
2023-11-08 21:15:44 +0900

    [FEAT] : 같은 버스 정류장 끼리 연결해주는 환승 엣지 추가 구현

--- data/bus_data_processing/transfer_link/transfer_link.py
+++ data/bus_data_processing/transfer_link/transfer_link.py
@@ -11,20 +11,27 @@ import json
 # generateTransferBusEdge()
 # 5. 이때 생성된 각각의 버스 노드들끼리 연결하는 엣지를 생성해준다.(환승 엣지로 사용될 것이다.)
 
-# 적어놓기
+# Memo
 # 기존 버스 정류장 노드의 id 범위는 100000001 ~ 717104358
+# 기존 버스 정류장 노드의 개수는 61809개
+# 기존 버스 엣지의 개수는 ???
+# 환승이 적용된 버스 정류장 노드의 id 범위는 1000000001 ~ 1000091159 (기존 보다 한자리 수 더 많음!!!)
+# 환승이 적용된 버스 정류장 노드의 개수는 ???
+# 환승이 적용된 엣지의 개수는 ???
+
 
 
 def generateTransferBusNode():
     
+    bus_stop_filePath = 'bus_stop_node.json'
+    bus_route_filePath = 'bus_router_edge.json'
+
     bus_stop_node_with_transfer_index = 1000000001
     
     # 각각의 노선별로 나눠진 정류장 노드가 들어가게 되는 새로운 버스 정류장 노드 
     bus_stop_node_with_transfer = []
 
-    bus_stop_filePath = 'bus_stop_node.json'
-    bus_route_filePath = 'bus_router_edge.json'
-
+    # 진행도
     progress_idx = 0
 
     with open(bus_stop_filePath, 'r', encoding='utf-8') as f:
@@ -79,7 +86,47 @@ def generateTransferBusNode():
 
 
 def generateTransferBusEdge():
-    pass
+
+    bus_stop_filePath = 'bus_stop_node_with_transfer.json'
+    bus_route_filePath = 'bus_router_edge.json'
+
+    with open(bus_stop_filePath, 'r', encoding='utf-8') as f:
+        bus_stop_list = json.load(f)
+
+    with open(bus_route_filePath, 'r', encoding='utf-8') as f:
+        bus_route_list = json.load(f) 
+    
+    # 정류장의 prev_bus_stop_id를 기준으로 그룹화
+    bus_stop_group = {}
+    for bus_stop in bus_stop_list:
+        if bus_stop['prev_bus_stop_id'] not in bus_stop_group:
+            bus_stop_group[bus_stop['prev_bus_stop_id']] = []
+        bus_stop_group[bus_stop['prev_bus_stop_id']].append(bus_stop)
+
+    # 서로서로 연결해주는 edge 생성
+    for bus_stop_id, bus_stop_nodes in bus_stop_group.items():
+        for i in range(len(bus_stop_nodes)-1):
+            for j in range(i+1, len(bus_stop_nodes)):
+                bus_route_list.append({
+                    "start" : bus_stop_nodes[i]['id'],
+                    "end" : bus_stop_nodes[j]['id'],
+                    "cost" : 20.0,
+                    "type" : "BUS",
+                    "route_id" : 0,
+                    "line" : "환승",
+                })
+
+                # 반대 방향의 엣지도 추가
+                bus_route_list.append({
+                    "start": bus_stop_nodes[j]['id'],
+                    "end":  bus_stop_nodes[i]['id'],
+                    "cost" : 20.0,
+                    "type" : "BUS",
+                    "route_id" : 0,
+                    "line" : "환승",
+                })
+    
+    with open('bus_router_edge_with_transfer.json', 'w', encoding='utf-8') as f:
+        json.dump(bus_route_list, f, indent=4, ensure_ascii=False)
 
 
-generateTransferBusNode()
