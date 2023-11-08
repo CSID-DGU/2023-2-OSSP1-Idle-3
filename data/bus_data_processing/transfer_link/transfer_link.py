1544c5ac470ccdfc8cf78795dc3fb20f9dc8a24e
seonwoonam <potatomoney716@gmail.com>
2023-11-08 20:33:49 +0900

    [FIX] : 3중 for문 구조 수정

--- data/bus_data_processing/transfer_link/transfer_link.py
+++ data/bus_data_processing/transfer_link/transfer_link.py
@@ -13,25 +13,26 @@ import json
 
 
 def generateTransferBusNode():
-
+    
     bus_stop_node_with_transfer_index = 1000000001
     
     # 각각의 노선별로 나눠진 정류장 노드가 들어가게 되는 새로운 버스 정류장 노드 
     bus_stop_node_with_transfer = []
 
-    # 새로운 버스 정류장 노드가 적용된 엣지
-    bus_route_edge_with_transfer = []
-
     bus_stop_filePath = 'bus_stop_node.json'
     bus_route_filePath = 'bus_router_edge.json'
+
+
 
     with open(bus_stop_filePath, 'r', encoding='utf-8') as f:
         bus_stop_list = json.load(f)
 
     with open(bus_route_filePath, 'r', encoding='utf-8') as f:
         bus_route_list = json.load(f)   
-
+  
     for bus_stop in bus_stop_list:
+
+
         bus_stop_id = bus_stop['id']
 
         # 해당 정류장의 id가 출발지 혹은 도착지에 들어있는 버스노선 들을 찾고 route_id 별로 묶는다
@@ -42,35 +43,31 @@ def generateTransferBusNode():
                     bus_route_group[bus_route['route_id']] = []
                 bus_route_group[bus_route['route_id']].append(bus_route)
 
-            # 찾은 버스노선의 갯수만큼 버스 노드를 생성해준다. 
-            for route_id, bus_routes in bus_route_group.items():
-                bus_stop_node_with_transfer.append({
-                    "id" : bus_stop_node_with_transfer_index,
-                    "position" : {
-                        "latitude" : bus_stop['position']['latitude'],
-                        "longitude" : bus_stop['position']['longitude'],
-                    },
-                    "name" : bus_stop['name'] + "_" + bus_route['line'],
-                })
+        # 찾은 버스노선의 갯수만큼 버스 노드를 생성해준다. 
+        for route_id, bus_routes in bus_route_group.items():
+            bus_stop_node_with_transfer.append({
+                "id" : bus_stop_node_with_transfer_index,
+                "position" : {
+                    "latitude" : bus_stop['position']['latitude'],
+                    "longitude" : bus_stop['position']['longitude'],
+                },
+                "name" : bus_stop['name'] + "_" + bus_routes[0]['line'],
+            })
 
-                #생성된 버스 노드 id를 이용하여 엣지의 기존 출발지 혹은 도착지 id를 수정해준다.
-                for bus_route in bus_routes:
-                    if bus_stop_id == bus_route['start']:
-                        bus_route['start'] = bus_stop_node_with_transfer_index
-                    elif bus_stop_id == bus_route['end']:
-                        bus_route['end'] = bus_stop_node_with_transfer_index
+            #생성된 버스 노드 id를 이용하여 bus_route_list엣지의 기존 출발지 혹은 도착지 id를 수정해준다.
+            for bus_route in bus_route_list:
+                if bus_stop_id == bus_route['start']:
+                    bus_route['start'] = bus_stop_node_with_transfer_index
+                elif bus_stop_id == bus_route['end']:
+                    bus_route['end'] = bus_stop_node_with_transfer_index
 
-                    # 수정된 엣지를 새로운 엣지 리스트에 추가해준다.
-                    bus_route_edge_with_transfer.append(bus_route)
 
-                bus_stop_node_with_transfer_index += 1
+            bus_stop_node_with_transfer_index += 1
 
     
     with open('bus_stop_node_with_transfer.json', 'w', encoding='utf-8') as f:
         json.dump(bus_stop_node_with_transfer, f, indent=4, ensure_ascii=False)
 
-    with open('bus_route_edge_with_transfer.json', 'w', encoding='utf-8') as f:
-        json.dump(bus_route_edge_with_transfer, f, indent=4, ensure_ascii=False)
 
 
 def generateTransferBusEdge():
