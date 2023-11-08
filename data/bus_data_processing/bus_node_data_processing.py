14df1c629743e4910587fce0aa5f36cc7ee43f93
seonwoonam <potatomoney716@gmail.com>
2023-11-08 18:35:50 +0900

    [FIX] : 버스 정류장 데이터 전처리 수정

--- data/bus_data_processing/bus_node_data_processing.py
+++ data/bus_data_processing/bus_node_data_processing.py
@@ -1,22 +1,24 @@
 import json
+import csv
 
 def getBusStopNodeJson():
-    bus_stop_filePath = "rawData/bus_stop.json" 
+    bus_stop_filePath = "rawData/bus_stop_info.csv" 
 
-    with open(bus_stop_filePath, 'r', encoding='utf-8') as f:
-        bus_stops_list = json.load(f)
+    with open(bus_stop_filePath, 'r', encoding='utf-8-sig') as csv_file:
+        reader = csv.DictReader(csv_file)
+        bus_stops_list = list(reader)
 
     # 버스 정류장(bus stop) 노드
     node_bus_stop_json_list = [
         {
-            "id": bus_stop["sttn_id"],
+            "id": int(bus_stop["\ufeff정류장ID"].replace("\ufeff", "")),
             "position": {
-                "latitude": bus_stop["crdnt_y"],
-                "longitude": bus_stop["crdnt_x"]
+                "latitude": float(bus_stop["좌표Y"].replace("\ufeff", "")),
+                "longitude": float(bus_stop["좌표X"].replace("\ufeff", ""))
             },
-            "name": bus_stop["sttn_nm"]
+            "name": bus_stop["정류장명"].replace("\ufeff", "")
         }
-        for bus_stop in bus_stops_list['DATA']
+        for bus_stop in bus_stops_list
     ]
 
     with open('bus_stop_node.json', 'w', encoding='utf-8') as f:
@@ -25,3 +27,4 @@ def getBusStopNodeJson():
 
 
 getBusStopNodeJson()
+
