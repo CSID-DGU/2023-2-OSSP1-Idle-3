6733fd97a3e6f1ac8ef696219f0b954887e06671
likerhythm <wjdalsdl0412@gmail.com>
2023-11-14 20:40:55 +0900

    [FEAT]: 시각화 - 중구 노드 데이터만 추출하는 코드 추가

---
+++ data/visualize/make_map/data_processing/step_node_data_in_Joongu_processing.py
@@ -0,0 +1,36 @@
+import csv
+import json
+
+def getStepNodeJson():
+  data = []
+  with open("step_raw_data.csv", 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    data = list(reader)
+
+  node_reader, node_data = [], []
+
+  for i in range(len(data)):
+    if data[i]['NodeLink'] == 'NODE':
+      node_reader.append(data[i])
+
+  # 노드 변환
+  for i in range(len(node_reader)):
+    sigoongu = node_reader[i]["SiGoonGu Name"]
+    if sigoongu == "중구":
+      longitude, latitude = node_reader[i]['Node WKT'][6:-1].split()
+      n = {
+        'id': int(node_reader[i]['Node ID']) + 900000, # 지하철과 노드 id와 겹치지 않도록 90만 더하기
+        'crosswalk': int(node_reader[i]['Crosswalk']),
+        'position': {
+          'latitude': float(latitude),  # 위도
+          'longitude': float(longitude) # 경도
+        }
+      }
+      node_data.append(n)
+  print(node_data[0])
+  
+  # 노드 json 쓰기
+  with open("result_data/step_node_in_Joongu.json", 'w', encoding = 'utf-8') as json_file:
+    json.dump(node_data, json_file)
+
+getStepNodeJson()
