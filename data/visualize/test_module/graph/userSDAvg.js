b28d907add74b560d84699a6a66bc7fd696e025a
Eunse Jang <senuej37@gmail.com>
2023-12-02 21:28:06 +0900

    [FIX] : N각형 표준편차 그래프

--- data/visualize/test_module/graph/userSDAvg.js
+++ data/visualize/test_module/graph/userSDAvg.js
@@ -1,69 +1,60 @@
 const fs = require("fs");
 const { plot } = require("nodeplotlib");
 
+const inputFilePath = "input.json";
+
 function processData(inputData) {
   const resultData = {};
 
   for (const entry of inputData) {
     const userTotalCnt = entry.userTotalCnt;
     const userInShapeCnt = entry.userInShapeCnt;
-    const userSDAvg = entry.userSDAvg;
+    const serverResponse = entry.serverResponse;
 
     if (!resultData[userTotalCnt]) {
       resultData[userTotalCnt] = {};
     }
 
     if (!resultData[userTotalCnt][userInShapeCnt]) {
-      resultData[userTotalCnt][userInShapeCnt] = { sum: 0, count: 0 };
+      resultData[userTotalCnt][userInShapeCnt] = {
+        sum: 0,
+        count: 0,
+      };
     }
-
-    resultData[userTotalCnt][userInShapeCnt].sum += userSDAvg;
+    resultData[userTotalCnt][userInShapeCnt].sum += serverResponse.SDAvg;
     resultData[userTotalCnt][userInShapeCnt].count += 1;
   }
 
-  const averages = {};
-
-  for (const userTotalCnt in resultData) {
-    averages[userTotalCnt] = {};
-
-    for (const userInShapeCnt in resultData[userTotalCnt]) {
-      averages[userTotalCnt][userInShapeCnt] =
-        resultData[userTotalCnt][userInShapeCnt].sum /
-        resultData[userTotalCnt][userInShapeCnt].count;
-    }
-  }
-
-  return averages;
+  return resultData;
 }
 
 function generateGraph(data) {
-  const plots = [];
-
   for (const userTotalCnt in data) {
-    const xValues = Object.keys(data[userTotalCnt]);
-    const yValues = Object.values(data[userTotalCnt]);
+    const plotData = [];
 
-    plots.push({
-      x: xValues,
-      y: yValues,
-      type: "line",
-      name: `userTotalCnt=${userTotalCnt}`,
+    for (const userInShapeCnt in data[userTotalCnt]) {
+      const avgUserSDAvg =
+        data[userTotalCnt][userInShapeCnt].sum /
+        data[userTotalCnt][userInShapeCnt].count;
+      plotData.push({
+        x: userInShapeCnt,
+        y: avgUserSDAvg,
+        type: "bar",
+        name: `userInShapeCnt=${userInShapeCnt}`,
+      });
+    }
+
+    plot(plotData, {
+      xaxis: { title: "userInShapeCnt" },
+      yaxis: { title: "Average userSDAvg" },
+      title: `userTotalCnt=${userTotalCnt}`,
     });
   }
-
-  plot(plots, {
-    xaxis: { title: "userInShapeCnt" },
-    yaxis: { title: "Average userSDAvg" },
-    title: "Average userSDAvg vs userInShapeCnt for different userTotalCnt",
-  });
 }
 
 function main() {
-  const inputData = JSON.parse(fs.readFileSync("input.json", "utf-8"));
-
+  const inputData = JSON.parse(fs.readFileSync(inputFilePath, "utf-8"));
   const processedData = processData(inputData);
-
-  fs.writeFileSync("userSDAvg.json", JSON.stringify(processedData, null, 2));
 
   generateGraph(processedData);
 }
