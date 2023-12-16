05a9758fd919c8b3d8083ebce9aab97458b121af
seonwoonam <potatomoney716@gmail.com>
2023-12-16 18:29:19 +0900

    FEAT : findTransportationTime 함수 추가

---
+++ data/odsay/findTransportationTime.js
@@ -0,0 +1,10 @@
+import wait from './wait.js';
+
+async function findTransportationTime() {
+    // input 
+    // 1. 출발지들 위도 경도
+    // 2. 도착지 한 곳의 위도 경도
+
+    // output
+    // 각 출발지에서 도착지까지의 걸리는 시간을 리스트로 반환(출발지 순서대로)
+}
